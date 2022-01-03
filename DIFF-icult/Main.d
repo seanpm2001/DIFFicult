8a5b5b97ce8cb97444eb13f43081f6d98ee7adc4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 15:50:07 -0800

    Main.d (V1)
    Version 1 of the main D file for this project.

---
+++ DIFF-icult/Main.d
@@ -0,0 +1,44 @@
+#!/usr/bin/rdmd
+// Start of script
+/*
+Main.d
+The main thread for the Diff IDE Known as DIFFicult
+This file houses all the called instructions for the editor. Headers, graphics, and other files are bundled separetely.
+*/
+// Note: I am not very experienced with the D programming language, so for now, this is mainly pseudocode
+void diff() {
+  // This section is coming soon
+  break;
+}
+void readDiff() {
+  writefln("Reading DIFF file...");
+  // This section is coming soon
+  break;
+}
+void editDiff() {
+  // This section is coming soon
+  break;
+}
+void main() {
+  return diff(); // Main Diff compoents
+  return readDiff(); // To read DIFF files
+  return editDiff(); // To edit DIFF files
+  break;
+}
+return main()
+Thread.sleep( dur!("seconds")( 5 ) ); // sleep for 5 seconds
+if Thread.response != 1 {
+  return 0;
+  break;
+} else {
+  continue;
+  break;
+}
+return 0;
+break;
+/* File info
+* File type: D source code (*.d)
+* File version: 1 (2022, Monday, January 3rd at 3:50 pm)
+* Line count (including blank lines and compiler line): 45
+*/
+// End of script
