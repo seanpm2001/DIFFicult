773cf4d325ef3978c7471f40fee8708e8663dfb2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 16:21:06 -0800

    About.d (V1)
    Version 1 of the about page for this project.

---
+++ DIFF-icult/About.d
@@ -0,0 +1,27 @@
+#!/usr/bin/rdmd
+// Start of script
+/*
+About.d
+The about page for the Diff IDE Known as DIFFicult
+*/
+// Note: I am not very experienced with the D programming language, so for now, this is mainly pseudocode
+void about() {
+  writefln("About DIFFicult\n")
+  writefln("DIFFicult - The DIFF editor\n")
+  writefln("Version 0.0.1 - 2022 January 3rd")
+  writefln("Edit and view DIFF files on Linux, MacOS, Windows, BSD, and more with this advanced, FLOSS Diff manager.")
+  writefln("Written in: D, R, YAML, Markdown, Diff, INI, and plain text")
+  writefln("Started on GitHub at https://github.com/seanpm2001/DIFFicult/")
+  writefln("Copyleft 🄯 Seanpm2001 (2022-2022)")
+  writefln("You should have received a copy of the GNU General Public license with this software. If not, you can get a copy here: https://www.gnu.org/licenses/gpl-3.0.html ")
+  break;
+}
+return about()
+return 0;
+break;
+/* File info
+* File type: D source code (*.d)
+* File version: 1 (2022, Monday, January 3rd at 4:21 pm)
+* Line count (including blank lines and compiler line): 28
+*/
+// End of script
