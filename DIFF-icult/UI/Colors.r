bc058e611293ffcc4ae357b07eca7e3f0a2386b4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 18:57:02 -0800

    Colors.r (V1)
    The current version (version 1) of the colors file for this project.

---
+++ DIFF-icult/UI/Colors.r
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+# Start of script
+# Colored text with Diff editing (syntax highlighting)
+# Please note that this is mostly pseudocode, as I am highly inexperienced with the R programming language
+x = * # x currently is just a dead value that stands for everything
+synhighP <- hex(x("#001100")); # Hex code for Green
+synhighN <- hex(x("#FF0000")); # Hex code for Red
+# synhighP stands for Syntax Highlighting Positive, and is used for +/++
+# synhighN stands for Syntax Highlighting Negative, and is used for -/--
+return <- function(synhighP);
+return <- function(synhighN);
+break;
+# File info
+# File type: R Source file (*.r)
+# File version: 1 (2022, Monday, January 3rd at 6:57 pm)
+# Line count (including blank lines and compiler line): 18
+# End of script
