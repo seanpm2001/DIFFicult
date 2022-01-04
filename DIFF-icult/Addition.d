e5543b707dc85354713ae4938d7721ad2514300c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 18:48:01 -0800

    Addition.d (V1)
    Version 1 of the addition script for this program.

---
+++ DIFF-icult/Addition.d
@@ -0,0 +1,30 @@
+#!/usr/bin/rdmd
+// Start of script
+/*
+Addition.d
+A script for adding to diff data (++) opposed to `/deduction.d` which subtracts from diff data (--)
+*/
+// Note: I am not very experienced with the D programming language, so for now, this is mainly pseudocode
+void main() {
+  writefln("Preparing to add...");
+  return addTest();
+  return altAddTest();
+  break;
+}
+void addTest() {
+  Thread.write("+");
+  break;
+}
+void altAddTest() {
+  Thread.write("++");
+  break;
+}
+return main()
+return 0;
+break;
+/* File info
+* File type: D source code (*.d)
+* File version: 1 (2022, Monday, January 3rd at 6:48 pm)
+* Line count (including blank lines and compiler line): 31
+*/
+// End of script
