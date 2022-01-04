1417490dba8c9db01c3d87c92b71d662df99c471
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 16:09:44 -0800

    Main_V1.r
    An archive of the first version of the main R file for this directory.

---
+++ DIFF-icult/UI/OldVersions/Main.r/1/1-100/Main_V1.r
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+# Start of script
+# The UI for the DIFFicult DIFF editor
+# Please note that this is mostly pseudocode, as I am highly inexperienced with the R programming language
+x = * # x currently is just a dead value that stands for everything
+uxi <- function(x) {
+  matrix displayRes1(1920,1080)
+}
+utoolbar <- function(x) {
+  matrix item1("Menu") {
+    item1.file("[Save], [Save as], [Edit], [Load], [Open], [View], [Debug], [Lint], [Run test]")
+  }
+}
+return <- function(uxi)
+# File info
+# File type: R Source file (*.r)
+# File version: 1 (2022, Monday, January 3rd at 4:09 pm)
+# Line count (including blank lines and compiler line): 20
+# End of script
