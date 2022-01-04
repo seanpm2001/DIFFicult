bfc33bfd93b3197c2867ea842969c78d77ef8ec5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 18:51:07 -0800

    Deduction.d (V1)
    Version 1 of the Deduction script for this project.

---
+++ DIFF-icult/Deduction.d
@@ -0,0 +1,31 @@
+#!/usr/bin/rdmd
+// Start of script
+/*
+Deduction.d
+A script for subtracting from diff data (--) opposed to `/ddition.d` which adds to diff data (++)
+*/
+// Note: I am not very experienced with the D programming language, so for now, this is mainly pseudocode
+void main() {
+  writefln("Preparing to deduct...");
+  return dedTest();
+  return altDedTest();
+  break;
+}
+void dedTest() {
+  Thread.write("-");
+  break;
+}
+void altDedTest() {
+  Thread.write("--");
+  break;
+}
+// Question: is the format for diff + & - or ++ & --
+return main()
+return 0;
+break;
+/* File info
+* File type: D source code (*.d)
+* File version: 1 (2022, Monday, January 3rd at 6:51 pm)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
