d712f6033258c4e4544068abd713c40c3f6a6885
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-04 17:08:29 -0800

    Headers_V1.h
    An archive of the first version of the headers file for this project.

---
+++ DIFF-icult/OldVersions/Headers.h/1/1-100/Headers_V1.h
@@ -0,0 +1,16 @@
+// Start of script
+// Headers file for DIFFicult
+
+// D Headers
+#include <About.d>
+#include <Addition.d>
+#include <Deduction.d>
+#include <Main.d>
+
+/* File info
+* File type: C header file (*.h)
+* File version: 1 (2021, Tuesday, January 4th at 5:08 pm)
+* Line count (including blank lines and compiler line): 17
+*/
+
+// End of script
